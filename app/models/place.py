13ee906395c954ab0dba2b8bd18a19b152fc22ff
bluelightwatcher <9550@holbertonstudents.com>
2024-10-27 16:57:59 +0100

    updating owner_check in place class, create_place in facade, and post request in places

--- app/models/place.py
+++ app/models/place.py
@@ -1,6 +1,7 @@
 from app.models.base_model import BaseModel
 from app.models.user import User
 
+
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
@@ -9,20 +10,10 @@ class Place(BaseModel):
         self.price = self.price_check(price)
         self.latitude = self.latitude_check(latitude)
         self.longitude = self.longitude_check(longitude)
-        # self.owner_id = self.owner_check(owner_id)
         self.owner = self.owner_check(owner)
         self.reviews = []  # List to store related reviews
         self.amenities = []  # List to store related amenities
     
-    def to_dict(self):
-        result = {}
-        for key, value in self.__dict__.items():
-            if isinstance(value, list):
-                result[key] = [item.to_dict() if hasattr(item, 'to_dict') else item for item in value]
-            else:
-                result[key] = value.to_dict() if hasattr(value, 'to_dict') else value
-        return result
-
     @staticmethod
     def price_check(price):
         if price < 0:
@@ -49,18 +40,15 @@ class Place(BaseModel):
             raise ValueError("longitude not correct")
         return longitude
        
-    @staticmethod
-    def owner_check(owner):
+    def owner_check(self,owner):
         if not isinstance(owner, User):
             raise ValueError(f"Owner {owner} must be a valid User instance")
         return owner
         
-    @classmethod
     def add_review(self, review):
         """Add a review to the place."""
         self.reviews.append(review)
 
-    @classmethod
     def add_amenity(self, amenity):
         """Add an amenity to the place."""
         self.amenities.append(amenity)
