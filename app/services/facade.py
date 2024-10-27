e5026b70590676f102b51aec24ae13988bb592e8
bluelightwatcher <9550@holbertonstudents.com>
2024-10-27 18:45:38 +0100

    get all and get by id method added

--- app/services/facade.py
+++ app/services/facade.py
@@ -90,14 +90,14 @@ class HBnBFacade:
 
     def create_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
         return amenity
 
     def get_amenity(self, amenity_id):
-        return self.amenity_repo(amenity_id)
+        return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-    # Placeholder for logic to retrieve all amenities
-        pass
+        return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
     # Placeholder for logic to update an amenity
