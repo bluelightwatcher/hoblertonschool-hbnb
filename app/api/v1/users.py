e9358cfc9b6e0705f30598d1f2df6829860e72e3
bluelightwatcher <9550@holbertonstudents.com>
2024-10-29 15:43:09 +0100

    Udpating error response message

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,14 +1,16 @@
 from flask_restx import Namespace, Resource, fields, marshal
 from app.services.facade import facade
-from flask import Flask, jsonify
+from werkzeug.exceptions import BadRequest
+
 
 api = Namespace('users', description='User operations')
+error = BadRequest('Invalid input data')
 
 # Define the user model for input validation and documentation
 user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'email': fields.String(required=True, description='Email of the user',)
 })
 user_response_model = api.model('User_response', {
     'id' : fields.String(required=True, description='Id of the user inherited from base model'),
@@ -17,24 +19,22 @@ user_response_model = api.model('User_response', {
     'email': fields.String(required=True, description='Email of the user')
 })
 
-
 @api.route('/')
 class UserList(Resource):
     @api.expect(user_model, validate=True)
     @api.response(201, 'User successfully created')
     @api.response(400, 'Email already registered')
     @api.response(400, 'Invalid input data')
+
     def post(self):
         """Register a new user"""
         user_data = api.payload
-
-        # Simulate email uniqueness check (to be replaced by real validation
-        # with persistence)
-        existing_user = facade.get_user_by_email(user_data['email'])
-        if existing_user:
-            return {'error': 'Email already registered'}, 400
-
-        new_user = facade.create_user(user_data)
+        if facade.get_user_by_email(user_data['email']):
+            raise error
+        try:
+            new_user = facade.create_user(user_data)
+        except (TypeError, ValueError):
+            raise error
         return marshal(new_user, user_response_model), 201
 
 
@@ -47,7 +47,7 @@ class UserResource(Resource):
         """Get user details by ID"""
         user = facade.get_user(user_id)
         if not user:
-            return {'error': 'User not found'}, 404
+            raise error 
         else:
             return marshal(user, user_response_model), 201
     
@@ -62,6 +62,6 @@ class UserResource(Resource):
 
         user = facade.update_user(user_id, user_data)
         if not user:
-            return {'error': 'User not found'}, 404
+            raise error
         else:
             return user
--- app/models/user.py
+++ app/models/user.py
@@ -19,7 +19,7 @@ class User(BaseModel):
                 elif not isinstance (first_name, str):
                         raise TypeError("Name must be a string")
                 return first_name
-        
+                        
         @staticmethod
         def last_name_check(last_name):
                 if  len(last_name) > 50:
@@ -33,14 +33,13 @@ class User(BaseModel):
         def email_check(email):
                 regex = r'^[a-zA-Z0-9.+-]+@[a-zA-Z]+\.[a-zA-Z0-9]{2,}+$'
                 if not re.match(regex, email):
-                    raise ValueError("Email must follow standard email format.")
+                    raise ValueError
                 return email 
+        
        
         
-        @classmethod
         def add_place(self, place):
                self.places.append(place)
 
-        @classmethod
         def add_review(self, review):
                self.reviews.append(review)
